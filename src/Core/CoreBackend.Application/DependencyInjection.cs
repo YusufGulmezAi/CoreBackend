dcac97418c514b361f9970404c6cb8994d930107
YusufGulmez <yusuf.gulmez.ai@gmail.com>
2026-01-31 13:39:06 +0300

    Add Application DependencyInjection configuration

---
+++ src/Core/CoreBackend.Application/DependencyInjection.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using CoreBackend.Application.Common.Behaviors;
+using CoreBackend.Application.Common.Interfaces;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CoreBackend.Application;
+
+/// <summary>
+/// Application katmanı dependency injection kayıtları.
+/// </summary>
+public static class DependencyInjection
+{
+	public static IServiceCollection AddApplication(this IServiceCollection services)
+	{
+		var assembly = Assembly.GetExecutingAssembly();
+
+		// MediatR
+		services.AddMediatR(cfg =>
+		{
+			cfg.RegisterServicesFromAssembly(assembly);
+		});
+
+		// FluentValidation - tüm validator'ları otomatik kaydet
+		services.AddValidatorsFromAssembly(assembly);
+
+		// MediatR Pipeline Behaviors (sıralama önemli!)
+		services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+		services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
+		services.AddTransient(typeof(IPipelineBehavior<,>), typeof(TransactionBehavior<,>));
+
+		// Business Rule Checker
+		services.AddScoped<IBusinessRuleChecker, BusinessRuleChecker>();
+
+		return services;
+	}
+}
